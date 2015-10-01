7e9181764c78c84ae60954f63159a5465d517430
[iMomen] <coder@imomen.com>
2015-10-01 14:24:51 +0300

    Feat: Update the markers to be filterd on user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,85 +1,105 @@
+// A list of my prefered locations
 var myLocations = [{
-  name: "Petronas Twin Tower",
-  zip: "50088",
-  city: "Kuala Lumpur",
-  latlng: new google.maps.LatLng(3.15706, 101.71169),
+  name: "Alexis Bangsar",
+  zip: "59100",
+  address: "29 Jalan Telawi 3",
+  latlng: new google.maps.LatLng(3.13186, 101.67098),
   marker: null
 }, {
   name: "Bangsar Village Shopping Centre",
-  zip: "59100",
-  city: "Kuala Lumpur",
+  zip: "59101",
+  address: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.13243, 101.67046),
   marker: null
 }, {
   name: "Damansara Heights",
   zip: "50490",
-  city: "Kuala Lumpur",
+  address: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.15219, 101.67090),
   marker: null
 }, {
   name: "Perdana Botanical Garden",
-  street: "Jalan Raja Abdullah",
+  address: "Jalan Raja Abdullah",
   zip: "50300",
-  city: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.143, 101.68466),
   marker: null
 }, {
   name: "Desa Park City",
-  street: "The Waterfront",
+  address: "The Waterfront",
   zip: "52200",
-  city: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.18532, 101.6319984),
   marker: null
 }, {
   name: "Restoran Kin Kin",
-  street: "40, Jalan Dewan Sultan Sulaiman, Kampung Baru",
+  address: "40, Jalan Dewan Sultan Sulaiman, Kampung Baru",
   zip: "50300",
-  city: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.1606721, 101.6982911),
   marker: null
 }, {
   name: "VCR",
-  street: "2, Jalan Galloway, Bukit Bintang",
+  address: "2, Jalan Galloway, Bukit Bintang",
   zip: "50150",
-  city: "Kuala Lumpur",
   latlng: new google.maps.LatLng(3.143182, 101.705589),
   marker: null
-}, {
-  name: "Aunty Nat",
-  street: "7, Jalan Sri Hartamas 7, Taman Sri Hartamas",
-  zip: "50480",
-  city: "Kuala Lumpur",
-  latlng: new google.maps.LatLng(3.1571721, 101.6503946),
-  marker: null
 }];
+
+// Create infoWindow content
+function createContent(place) {
+  var infoWindowContent = '<div class="infoWindow-content">';
+  infoWindowContent += '<h4 class="title">' + place.name + '</h4>';
+  infoWindowContent += '<div class="review"><img class="thumb" src="' + place.image_url + '">';
+  infoWindowContent += '<div class="review-content"><p>' + place.snippet_text + '</p>';
+  infoWindowContent += '<p><strong>Phone: </strong>' + place.display_phone + '</p>';
+  infoWindowContent += '<p><strong>Rating: </strong><img src="' + place.rating_img_url_small + '"></p>';
+  infoWindowContent += '</div></div></div>';
+  return infoWindowContent;
+}
 
 var AppViewModel = function() {
   var self = this;
   var map;
+  var marker;
   var service;
   var infowindow;
+  var placeLocation = [];
   self.points = ko.observableArray(myLocations);
 
   var mapCenter = new google.maps.LatLng(3.10048, 101.42577);
 
   var markersArray = [];
 
+  // initialize the map
   function initialize() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: mapCenter,
       zoom: 11
     });
 
-    var list = (document.getElementById('list'));
-    map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(list);
-    var input = (document.getElementById('pac-input'));
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    var searchBox = new google.maps.places.SearchBox((input));
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+      if (places.length == 0) {
+        return;
+      }
       var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+      var placeName = place.formatted_address;
+      self.getYelpData(placeName, 10);
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
       map.fitBounds(bounds);
     });
 
@@ -94,8 +114,9 @@ var AppViewModel = function() {
 
   }
 
+  // Create marker for every place on myLocations array
   function createMarker(place) {
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
       map: map,
       icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
       name: place.name.toLowerCase(),
@@ -104,22 +125,23 @@ var AppViewModel = function() {
       animation: google.maps.Animation.DROP
     });
 
-    var contentString = '<div style="font-weight: bold">' + place.name + '</div><div>' + place.city + '</div>';
-
     google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.setContent(contentString);
-      infoWindow.open(map, this);
+      self.getYelpData(place.address, 1);
+      infoWindow.setContent(createContent(place));
+      infoWindow.open(map, marker);
+      marker.setAnimation(google.maps.Animation.DROP);
       map.panTo(marker.position);
-      marker.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function() {
         marker.setAnimation(null);
       }, 1450);
     });
 
     markersArray.push(marker);
+    place.marker = marker;
     return marker;
   }
 
+  // Click on list items
   self.clickOnListItem = function(place) {
 
     var marker;
@@ -131,14 +153,9 @@ var AppViewModel = function() {
       }
     }
     map.panTo(marker.position);
-
-    setTimeout(function() {
-      var contentString = '<div style="font-weight: bold">' + place.name + '</div><div>' + place.city + '</div>';
-      infoWindow.setContent(contentString);
-      infoWindow.open(map, marker);
-      marker.setAnimation(google.maps.Animation.DROP);
-    }, 300);
-
+    self.getYelpData(place.address, 1);
+    infoWindow.open(map, marker);
+    marker.setAnimation(google.maps.Animation.DROP);
   };
 
   self.searchTerm = ko.observable('');
@@ -148,6 +165,66 @@ var AppViewModel = function() {
       return (point.name.toLowerCase().indexOf(self.searchTerm().toLowerCase()) >= 0);
     });
   });
+
+  self.search = ko.computed(function() {
+    return ko.utils.arrayFilter(self.points(), function(point) {
+
+      if (point.name.toLowerCase().indexOf(self.searchTerm().toLowerCase()) !== -1) {
+        if(point.marker) {
+          point.marker.setMap(map);
+        }
+      } else {
+        point.marker.setMap(null);
+      }
+      return (point.name.toLowerCase().indexOf(self.searchTerm().toLowerCase()) >= 0);
+    });
+  }, self);
+
+  // Get Yelp data for every location
+  self.getYelpData = function(placeAddress, resultsLimit) {
+    /**
+     * Generates a random number and returns it as a string for OAuthentication
+     * @return {string}
+     */
+    function nonce_generate() {
+      return (Math.floor(Math.random() * 1e12).toString());
+    }
+
+    var yelp_url = 'http://api.yelp.com/v2/search';
+
+    var parameters = {
+      oauth_consumer_key: 'Qo7AHPwy5OEC9xbJ7JQzAw',
+      oauth_token: 'EbHuZgmkfI9wFQEdjgzkGaqGiJoNrHvh',
+      oauth_nonce: nonce_generate(),
+      oauth_timestamp: Math.floor(Date.now() / 1000),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version: '1.0',
+      callback: 'cb',
+      term: 'food',
+      location: placeAddress,
+      limit: resultsLimit
+    };
+
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, 'KDOJMIocK-NQF2GTg7gT8i6xroQ', 'rg-VEIPz8m3VJ__85nlxyoNdSoQ');
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+      url: yelp_url,
+      data: parameters,
+      cache: true,
+      dataType: 'jsonp',
+      success: function(results) {
+        placeLocation = results.businesses;
+        var responseData = results.businesses[0];
+        infoWindow.setContent(createContent(responseData));
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    };
+
+    $.ajax(settings);
+  }
 
   google.maps.event.addDomListener(window, 'load', initialize);
 }
